f2e34e9c45c73f1ed76a6cd9685a918b3b0c7437
Keng-Wittaya <67319010051@tnk.ac.th>
2024-10-22 09:52:56 +0700

    Create wittaya.py

---
+++ python_week02/wittaya.py
@@ -0,0 +1,7 @@
+print(" ╔══════INTRODUCTION══════╗")
+print(" ‖ Name : Wittaya  Onkaew ‖")
+print(" ‖ Nickname : Keng        ‖")
+print(" ‖ No. : 10               ‖")
+print(" ‖ StdNo. : 67319010051   ‖")
+print(" ‖ Hobby : travel         ‖")
+print(" ╚════════════════════════╝")
